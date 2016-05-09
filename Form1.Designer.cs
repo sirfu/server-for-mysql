46624ba09e4f8395615f57f8e69b3cd896691ee8
sirfu <stopp8088@163.com>
2016-05-09 15:17:27 +0800

    no
    no

---
+++ Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace _11111
+{
+    partial class frm_server
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtIp = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.btnBeginListen = new System.Windows.Forms.Button();
+            this.txtMsg = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtMsgSend = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbOnline = new System.Windows.Forms.ListBox();
+            this.btnSendToAll = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(469, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "IP  :";
+            // 
+            // txtIp
+            // 
+            this.txtIp.Location = new System.Drawing.Point(510, 12);
+            this.txtIp.Name = "txtIp";
+            this.txtIp.Size = new System.Drawing.Size(91, 21);
+            this.txtIp.TabIndex = 1;
+            this.txtIp.Text = "192.168.1.10";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(469, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 12);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "PORT:";
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(510, 45);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(91, 21);
+            this.txtPort.TabIndex = 1;
+            this.txtPort.Text = "8088";
+            // 
+            // btnBeginListen
+            // 
+            this.btnBeginListen.Location = new System.Drawing.Point(471, 268);
+            this.btnBeginListen.Name = "btnBeginListen";
+            this.btnBeginListen.Size = new System.Drawing.Size(130, 42);
+            this.btnBeginListen.TabIndex = 2;
+            this.btnBeginListen.Text = "启动服务";
+            this.btnBeginListen.UseVisualStyleBackColor = true;
+            this.btnBeginListen.Click += new System.EventHandler(this.btnBeginListen_Click);
+            // 
+            // txtMsg
+            // 
+            this.txtMsg.BackColor = System.Drawing.Color.White;
+            this.txtMsg.Location = new System.Drawing.Point(7, 12);
+            this.txtMsg.Multiline = true;
+            this.txtMsg.Name = "txtMsg";
+            this.txtMsg.ReadOnly = true;
+            this.txtMsg.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtMsg.Size = new System.Drawing.Size(456, 239);
+            this.txtMsg.TabIndex = 3;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(471, 316);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(130, 42);
+            this.btnSend.TabIndex = 4;
+            this.btnSend.Text = "发送消息";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtMsgSend
+            // 
+            this.txtMsgSend.Location = new System.Drawing.Point(7, 270);
+            this.txtMsgSend.Multiline = true;
+            this.txtMsgSend.Name = "txtMsgSend";
+            this.txtMsgSend.Size = new System.Drawing.Size(455, 132);
+            this.txtMsgSend.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(467, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "在线列表";
+            // 
+            // lbOnline
+            // 
+            this.lbOnline.FormattingEnabled = true;
+            this.lbOnline.ItemHeight = 12;
+            this.lbOnline.Location = new System.Drawing.Point(471, 84);
+            this.lbOnline.Name = "lbOnline";
+            this.lbOnline.Size = new System.Drawing.Size(130, 172);
+            this.lbOnline.TabIndex = 7;
+            // 
+            // btnSendToAll
+            // 
+            this.btnSendToAll.Location = new System.Drawing.Point(471, 364);
+            this.btnSendToAll.Name = "btnSendToAll";
+            this.btnSendToAll.Size = new System.Drawing.Size(130, 42);
+            this.btnSendToAll.TabIndex = 8;
+            this.btnSendToAll.Text = "群发消息";
+            this.btnSendToAll.UseVisualStyleBackColor = true;
+            this.btnSendToAll.Click += new System.EventHandler(this.btnSendToAll_Click);
+            // 
+            // frm_server
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(618, 454);
+            this.Controls.Add(this.btnSendToAll);
+            this.Controls.Add(this.lbOnline);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtMsgSend);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtMsg);
+            this.Controls.Add(this.btnBeginListen);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtIp);
+            this.Controls.Add(this.label1);
+            this.MaximumSize = new System.Drawing.Size(626, 481);
+            this.MinimumSize = new System.Drawing.Size(626, 481);
+            this.Name = "frm_server";
+            this.Text = "物联网盒子服务端";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtIp;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Button btnBeginListen;
+        private System.Windows.Forms.TextBox txtMsg;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox txtMsgSend;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox lbOnline;
+        private System.Windows.Forms.Button btnSendToAll;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;  // IP，IPAddress, IPEndPoint，端口等；
+using System.Threading;
+using System.IO;
+using System.Data.SqlClient;
+using MySql.Data.MySqlClient; 
+
+namespace _11111
+{
+    public partial class frm_server : Form
+    {
+        public frm_server()
+        {
+            InitializeComponent();
+            TextBox.CheckForIllegalCrossThreadCalls = false;
+        }
+
+        Thread threadWatch = null; // 负责监听客户端连接请求的 线程；
+        Socket socketWatch = null;
+
+        Dictionary<string, Socket> dict = new Dictionary<string, Socket>();
+        Dictionary<string, Thread> dictThread = new Dictionary<string, Thread>();
+
+        private void btnBeginListen_Click(object sender, EventArgs e)
+        {
+            // 创建负责监听的套接字，注意其中的参数；
+            socketWatch = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            // 获得文本框中的IP对象；
+            IPAddress address = IPAddress.Parse(txtIp.Text.Trim());
+                // 创建包含ip和端口号的网络节点对象；
+                IPEndPoint endPoint = new IPEndPoint(address, int.Parse(txtPort.Text.Trim()));
+                try
+                {
+                    // 将负责监听的套接字绑定到唯一的ip和端口上；
+                    socketWatch.Bind(endPoint);
+                }
+                catch (SocketException se)
+                {
+                    MessageBox.Show("异常："+se.Message);
+                    return;
+                }
+                // 设置监听队列的长度；
+                socketWatch.Listen(10);
+                // 创建负责监听的线程；
+                threadWatch = new Thread(WatchConnecting);
+                threadWatch.IsBackground = true;
+                threadWatch.Start();
+                ShowMsg("服务器启动监听成功！");
+            //}
+        }
+
+        /// <summary>
+        /// 监听客户端请求的方法；
+        /// </summary>
+        void WatchConnecting()
+        {
+            while (true)  // 持续不断的监听客户端的连接请求；
+            {
+                // 开始监听客户端连接请求，Accept方法会阻断当前的线程；
+                Socket sokConnection = socketWatch.Accept(); // 一旦监听到一个客户端的请求，就返回一个与该客户端通信的 套接字；
+                // 想列表控件中添加客户端的IP信息；
+                lbOnline.Items.Add(sokConnection.RemoteEndPoint.ToString());
+                // 将与客户端连接的 套接字 对象添加到集合中；
+                dict.Add(sokConnection.RemoteEndPoint.ToString(), sokConnection);
+                ShowMsg("客户端连接成功！");
+                Thread thr = new Thread(RecMsg);
+                thr.IsBackground = true;
+                thr.Start(sokConnection);
+                dictThread.Add(sokConnection.RemoteEndPoint.ToString(), thr);  //  将新建的线程 添加 到线程的集合中去。
+            }
+        }
+
+        void RecMsg(object sokConnectionparn)
+        {
+                byte[] ackMsgOK = System.Text.Encoding.Default.GetBytes("Y");
+                byte[] ackMsgERR = System.Text.Encoding.Default.GetBytes("error msg!");
+                Socket sokClient = sokConnectionparn as Socket;
+                while (true)
+                {
+                    // 定义一个128的缓存区；
+                    byte[] arrMsgRec = new byte[128];
+                    // 将接受到的数据存入到输入  arrMsgRec中；
+                    int length = -1;
+                    try
+                    {
+                        length = sokClient.Receive(arrMsgRec); // 接收数据，并返回数据的长度；
+                    }
+                    catch (SocketException se)
+                    {
+                        ShowMsg("异常：" + se.Message);
+                        // 从 通信套接字 集合中删除被中断连接的通信套接字；
+                        dict.Remove(sokClient.RemoteEndPoint.ToString());
+                        // 从通信线程集合中删除被中断连接的通信线程对象；
+                        dictThread.Remove(sokClient.RemoteEndPoint.ToString());
+                        // 从列表中移除被中断的连接IP
+                        lbOnline.Items.Remove(sokClient.RemoteEndPoint.ToString());
+                        break;
+                    }
+                    catch (Exception e)
+                    {
+                        ShowMsg("异常：" + e.Message);
+                        // 从 通信套接字 集合中删除被中断连接的通信套接字；
+                        dict.Remove(sokClient.RemoteEndPoint.ToString());
+                        // 从通信线程集合中删除被中断连接的通信线程对象；
+                        dictThread.Remove(sokClient.RemoteEndPoint.ToString());
+                        // 从列表中移除被中断的连接IP
+                        lbOnline.Items.Remove(sokClient.RemoteEndPoint.ToString());
+                        break;
+                    }
+
+                    string strMsg = ByteToHex(arrMsgRec,length); // 将接受到的字节数据转化成字符串；
+                    ShowMsg(strMsg);
+                    if ((arrMsgRec[0] == 0x3A) && (arrMsgRec[1] == 0x5C) && (arrMsgRec[2] == 0x3E) && (arrMsgRec[22] == 0x05) && (arrMsgRec[23] == 0xdc))// 表示接收到的是有效数据；
+                    {
+                        byte[] dtaddress = new byte[4];
+                        byte[] dtcode = new byte[4];
+                        string idstring = System.Text.Encoding.UTF8.GetString(arrMsgRec, 12, 10);// 将接受到的字节数据转化成字符串；
+                        Buffer.BlockCopy(arrMsgRec, 4, dtaddress, 0, 4);
+                        int dterraddr = System.BitConverter.ToInt32(dtaddress, 0)+40000;
+                        Buffer.BlockCopy(arrMsgRec, 8, dtcode , 0, 4);
+                        int dterrcode = System.BitConverter.ToInt32(dtcode, 0);
+                        
+ //                       bool ifAutoflag ;
+ //                       if (arrMsgRec[3] == 0) { ifAutoflag = true; }
+ //                       else { ifAutoflag = false; }
+                        string MyConn = "Data Source=localhost;Database=dianti_1;User Id=guest;Password=123456";//定义数据库连接参数
+                        MySqlConnection MyConnection = new MySqlConnection(MyConn);//定义一个数据连接实例
+                        MySqlDataAdapter myDataAdapter = new MySqlDataAdapter("select * from input", MyConnection);
+                        DataSet myDataSet = new DataSet();
+                        myDataAdapter.Fill(myDataSet);
+
+                        DataTable myTable = myDataSet.Tables[0];
+                        DataRow myRow = myTable.NewRow();
+
+                        myRow["termid"] = 1001 ;
+                        myRow["termaddress"] = "紫竹园6号楼" ;
+
+                        int dataflag = 0;
+                        switch (dterraddr)
+                        {
+                            case 40001: myRow["status"] = "40001:欠速故障";
+                                break;
+                            case 40002: myRow["status"] = "40002:超速故障";
+                                break;
+                            case 40003: myRow["status"] = "40003:反转故障";
+                                break;
+                            case 40004: myRow["descript"] = "40004:AST故障（失速）";
+                                break;
+                            case 40005: myRow["status"] = "40005:过电流";
+                                break;
+                            case 40006: myRow["status"] = "40006:过电压";
+                                break;
+                            case 40007: myRow["status"] = "40007:欠电压";
+                                break;
+                            case 40008: myRow["status"] = "40008:LB接触器故障";
+                                break;
+                            case 40009: myRow["status"] = "40009:接触器故障";
+                                break;
+                            case 40010: myRow["status"] = "40010:BK抱闸故障";
+                                break;
+                            default: dataflag =1;
+                                break;
+                        }
+
+                        DateTime dt = DateTime.Now;
+                        myRow["inputtime"] = dt;
+ //                       myRow["ifauto"] = ifAutoflag;
+                        if (dataflag == 0)
+                        {
+                            //myRow["id"] = 100; id若为“自动增长”，此处可以不设置，即便设置也无效
+                            myTable.Rows.Add(myRow);
+
+                            // 将DataSet的修改提交至“数据库”
+                            MySqlCommandBuilder mySqlCommandBuilder = new MySqlCommandBuilder(myDataAdapter);
+                            myDataAdapter.Update(myDataSet);
+                            sokClient.Send(ackMsgOK); //向客户端返回成功信息
+                        }
+                        /*
+                            foreach (DataRow myRow in myTable.Rows)
+                            {
+                                foreach (DataColumn myColumn in myTable.Columns)
+                                {
+                                    System.Diagnostics.Debug.WriteLine(myRow[myColumn]);	//遍历表中的每个单元格
+                                }
+                            }*/
+//                        dataGridView1.DataSource = myDataSet.Tables[0];
+
+
+
+                        myDataSet.Dispose();        // 释放DataSet对象
+                        myDataAdapter.Dispose();    // 释放SqlDataAdapter对象
+                        //            myDataReader.Dispose();     // 释放SqlDataReader对象
+                        MyConnection.Close();             // 关闭数据库连接
+                        MyConnection.Dispose();           // 释放数据库连接对象
+                    }
+                    else
+                    {
+//                        sokClient.Send(ackMsgERR);
+                    }
+                }     
+        }
+
+        void ShowMsg(string str)
+        {
+            txtMsg.AppendText(str + "\r\n");
+        }
+
+        public static string ByteToHex(byte[] comByte,int dataLen)
+       {
+            string returnStr = "";
+            if (comByte != null)
+            {
+              for (int i = 0; i < dataLen; i++)
+               {
+                   returnStr += comByte[i].ToString("X2") + " ";
+               }
+            }
+           return returnStr;   
+       }
+
+
+        // 发送消息
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            string strMsg = txtMsgSend.Text.Trim();
+            byte[] arrMsg = System.Text.Encoding.UTF8.GetBytes(strMsg); // 将要发送的字符串转换成Utf-8字节数组；
+//            byte[] arrSendMsg=new byte[arrMsg.Length+1];
+//            arrSendMsg[0] = 0; // 表示发送的是消息数据
+//            Buffer.BlockCopy(arrMsg, 0, arrSendMsg, 1, arrMsg.Length);
+            string strKey = "";
+            strKey = lbOnline.Text.Trim();
+            if (string.IsNullOrEmpty(strKey))   // 判断是不是选择了发送的对象；
+            {
+                MessageBox.Show("请选择你要发送的对象！！！");
+            }
+            else
+            {
+                dict[strKey].Send(arrMsg);// 解决了 sokConnection是局部变量，不能再本函数中引用的问题；
+                ShowMsg(strMsg);
+                txtMsgSend.Clear();
+            }
+        }
+
+        /// <summary>
+        /// 群发消息
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e">消息</param>
+        private void btnSendToAll_Click(object sender, EventArgs e)
+        {
+            string strMsg =  txtMsgSend.Text.Trim();
+            byte[] arrMsg = System.Text.Encoding.UTF8.GetBytes(strMsg); // 将要发送的字符串转换成Utf-8字节数组；
+
+//            byte[] arrSendMsg = new byte[arrMsg.Length + 1];
+//            arrSendMsg[0] = 0; // 表示发送的是消息数据
+//            Buffer.BlockCopy(arrMsg, 0, arrSendMsg, 1, arrMsg.Length);
+
+            foreach (Socket s in dict.Values)
+            {
+                s.Send(arrMsg);
+            }
+            ShowMsg(strMsg);
+            txtMsgSend.Clear();
+            ShowMsg(" 群发完毕～～～");
+        }
+    
+    }
+}
